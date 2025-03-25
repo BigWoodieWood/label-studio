e2c0b2d2554f2d85f855401738bd685cdc8868e1
Nick Skriabin <nr@fenelon.ru>
2025-03-25 11:02:44 +0000

    Organize imports

--- web/apps/labelstudio/src/pages/CreateProject/CreateProject.jsx
+++ web/apps/labelstudio/src/pages/CreateProject/CreateProject.jsx
@@ -1,22 +1,22 @@
 import { EnterpriseBadge } from "@humansignal/ui";
-import React, { useCallback } from "react";
+import React from "react";
 import { useHistory } from "react-router";
 import { Button, ToggleItems } from "../../components";
+import { Caption } from "../../components/Caption/Caption";
+import { Input, Select, TextArea } from "../../components/Form";
+import { HeidiTips } from "../../components/HeidiTips/HeidiTips";
+import { createURL } from "../../components/HeidiTips/utils";
 import { Modal } from "../../components/Modal/Modal";
 import { Space } from "../../components/Space/Space";
-import { HeidiTips } from "../../components/HeidiTips/HeidiTips";
 import { useAPI } from "../../providers/ApiProvider";
 import { cn } from "../../utils/bem";
+import { FF_LSDV_E_297, isFF } from "../../utils/feature-flags";
 import { ConfigPage } from "./Config/Config";
 import { EMPTY_CONFIG } from "./Config/Template";
 import "./CreateProject.scss";
-import { importFiles, ImportPage } from "./Import/Import";
+import { ImportPage } from "./Import/Import";
 import { useImportPage } from "./Import/useImportPage";
 import { useDraftProject } from "./utils/useDraftProject";
-import { Input, Select, TextArea } from "../../components/Form";
-import { Caption } from "../../components/Caption/Caption";
-import { FF_LSDV_E_297, isFF } from "../../utils/feature-flags";
-import { createURL } from "../../components/HeidiTips/utils";
 
 const ProjectName = ({ name, setName, onSaveName, onSubmit, error, description, setDescription, show = true }) =>
   !show ? null : (
