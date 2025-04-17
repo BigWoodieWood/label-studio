dde5f1a6bd2e5874eee8ec4a4762e44f7fa9870e
hakan458 <hakan@heartex.com>
2025-04-17 16:36:38 -0700

    remove print

--- label_studio/io_storages/all_api.py
+++ label_studio/io_storages/all_api.py
@@ -127,7 +127,6 @@ class AllImportStorageListAPI(generics.ListAPIView):
             response = view(request._request, *args, **kwargs)
             payload = response.data
             if not isinstance(payload, list):
-                print(f'{payload=}')
                 raise ValueError('Response is not list')
             return response.data
         except Exception:
