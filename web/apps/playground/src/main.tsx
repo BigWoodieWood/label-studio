b422440b67d2c10eb342974dbaad210b6dcce7f1
Brandon Martel <brandonmartel@gmail.com>
2025-05-05 12:56:58 -0500

    feat: OPTIC-1799: LabelStudio Playground 2.0

---
+++ web/apps/playground/src/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const PlaygroundApp = () => <div>LabelStudio Playground coming soon...</div>;
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<PlaygroundApp />);
