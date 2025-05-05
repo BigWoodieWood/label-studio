a3ee374ec176eecf4567851ec4f0f2300767a5c3
Brandon Martel <brandonmartel@gmail.com>
2025-05-05 16:15:50 -0500

    use the right styles

--- web/apps/playground/src/main.tsx
+++ web/apps/playground/src/main.tsx
@@ -2,5 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { PlaygroundApp } from "./components/PlaygroundApp";
 
+import "@humansignal/ui/src/styles.scss";
+import "@humansignal/ui/src/tailwind.css";
+
 const root = createRoot(document.getElementById("root")!);
 root.render(<PlaygroundApp />);
