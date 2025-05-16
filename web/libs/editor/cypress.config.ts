12bb64023142c9ed667a38dccdc3abef21f0fc57
Gondragos <gondragos@gmail.com>
2025-05-16 18:30:16 +0100

    fix: update Cypress config for targeted test specs
    Updated the Cypress configuration to include a specific specPattern for integration tests targeting files with ".only.cy" suffix. This change ensures only specific test files are executed during e2e testing. Renamed a test file to align with the updated specPattern.

--- web/libs/editor/cypress.config.ts
+++ web/libs/editor/cypress.config.ts
@@ -1,3 +1,9 @@
 import configure from "@humansignal/frontend-test/configure";
 
-export default configure();
+export default configure((defaultConfig) => ({
+  ...defaultConfig,
+  e2e: {
+    ...defaultConfig.e2e,
+    specPattern: "tests/integration/**/*.only.cy.{js,jsx,ts,tsx}",
+  },
+}));
