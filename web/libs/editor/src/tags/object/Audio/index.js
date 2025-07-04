54e642ddee121c43ec5db216d68573c8ffd69181
Brandon Martel <brandonmartel@gmail.com>
2025-07-04 11:53:03 -0500

    missed deleting an old import

--- web/libs/editor/src/tags/object/Audio/index.js
+++ web/libs/editor/src/tags/object/Audio/index.js
@@ -1,9 +1,7 @@
 import Registry from "../../../core/Registry";
 import { AudioModel } from "./model";
-import { HtxAudio } from "./view_old";
 import { AudioView as HtxAudio } from "./view";
 import { AudioRegionModel } from "../../../regions/AudioRegion";
-import { FF_DEV_1713, FF_DEV_2715, isFF } from "../../../utils/feature-flags";
 
 Registry.addTag("audio", AudioModel, HtxAudio);
 // @todo remove this once we have the ability to deprecate the old tag, for now allow the alias
