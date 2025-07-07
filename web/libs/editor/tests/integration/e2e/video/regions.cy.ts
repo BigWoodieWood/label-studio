e6c6a678427152408d952af47d3e504a7917b5f3
Brandon Martel <brandonmartel@gmail.com>
2025-07-07 15:57:18 -0500

    revert this test file to develop

--- web/libs/editor/tests/integration/e2e/video/regions.cy.ts
+++ web/libs/editor/tests/integration/e2e/video/regions.cy.ts
@@ -35,50 +35,50 @@ describe("Video segmentation", suiteConfig, () => {
     Sidebar.hasNoRegions();
 
     // Wait for video to be fully loaded and stable
-    VideoView.captureCanvas("segmentation-canvas");
+    VideoView.captureCanvas("canvas");
 
     Labels.select("Label 2");
     VideoView.drawRectRelative(0.2, 0.2, 0.6, 0.6);
 
     // Ensure drawing operations are complete before comparison
-    cy.wait(TWO_FRAMES_TIMEOUT);
+    cy.wait(1000);
 
     Sidebar.hasRegions(1);
 
-    VideoView.canvasShouldChange("segmentation-canvas", 0);
+    VideoView.canvasShouldChange("canvas", 0);
   });
-
-  it("Should be invisible out of the lifespan (rectangle)", () => {
+  // @todo: This test is flaky in CI, needs to be fixed
+  it.skip("Should be invisible out of the lifespan (rectangle)", () => {
     LabelStudio.params().config(simpleVideoConfig).data(simpleVideoData).withResult(simpleVideoResult).init();
     LabelStudio.waitForObjectsReady();
     // Wait for video and regions to be fully loaded
-    cy.wait(TWO_FRAMES_TIMEOUT);
+    cy.wait(1000);
 
     Sidebar.hasRegions(1);
 
-    VideoView.captureCanvas("rectangle-canvas");
+    VideoView.captureCanvas("canvas");
 
-    cy.wait(TWO_FRAMES_TIMEOUT);
+    cy.wait(1000);
 
     VideoView.clickAtFrame(4);
 
     // Ensure drawing operations are complete before comparison
-    cy.wait(TWO_FRAMES_TIMEOUT);
+    cy.wait(1000);
 
-    VideoView.canvasShouldChange("rectangle-canvas", 0);
+    VideoView.canvasShouldChange("canvas", 0);
   });
 
   it("Should be invisible out of the lifespan (transformer)", () => {
     LabelStudio.params().config(simpleVideoConfig).data(simpleVideoData).withResult(simpleVideoResult).init();
     LabelStudio.waitForObjectsReady();
     // Wait for frame change to be fully processed
-    cy.wait(TWO_FRAMES_TIMEOUT);
+    cy.wait(1000);
     Sidebar.hasRegions(1);
 
     cy.log("Remember an empty canvas state");
     VideoView.clickAtFrame(4);
-    cy.wait(TWO_FRAMES_TIMEOUT);
-    VideoView.captureCanvas("transformer-canvas");
+    cy.wait(1000);
+    VideoView.captureCanvas("canvas");
 
     VideoView.clickAtFrame(3);
     cy.wait(TWO_FRAMES_TIMEOUT);
@@ -91,8 +91,8 @@ describe("Video segmentation", suiteConfig, () => {
     cy.wait(TWO_FRAMES_TIMEOUT);
     Sidebar.hasSelectedRegions(1);
 
-    cy.wait(TWO_FRAMES_TIMEOUT);
+    cy.wait(1000);
 
-    VideoView.canvasShouldNotChange("transformer-canvas", 0);
+    VideoView.canvasShouldNotChange("canvas", 0);
   });
 });
