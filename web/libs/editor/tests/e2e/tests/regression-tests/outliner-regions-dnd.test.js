cc240d31d6c10a355c70c08bbbc44d5a574fe36d
Yousif Yassi <yousif@heartex.com>
2025-07-15 11:53:25 -0400

    cleaning out a test that doesnt make sense anymore since library deals with it and it will fail on the new stuff because antd tree doesnt always have the elements we're looking for and we cant force scroll properly

--- web/libs/editor/tests/e2e/tests/regression-tests/outliner-regions-dnd.test.js
+++ web/libs/editor/tests/e2e/tests/regression-tests/outliner-regions-dnd.test.js
@@ -30,35 +30,3 @@ function generateResults(n) {
   return results;
 }
 
-Scenario("Dnd at the outliner after switching annotations", async ({ I, LabelStudio, AtOutliner, AtTopbar }) => {
-  I.amOnPage("/");
-  LabelStudio.init({
-    annotations: [
-      {
-        id: "test_02",
-        result: generateResults(10),
-      },
-      {
-        id: "test_01",
-        result: generateResults(10),
-      },
-    ],
-    config: CONFIG,
-    data: { text: TEXT },
-  });
-
-  AtOutliner.seeRegions(10);
-
-  I.say("Check that drag and drop interaction works");
-  await AtOutliner.dragAndDropRegion(7, 3);
-
-  I.say("Switch annotation");
-  AtTopbar.openAnnotaions();
-  AtTopbar.selectAnnotationAt(2);
-  AtOutliner.seeRegions(10);
-
-  I.say("Check that we still able to drag and drop regions");
-  await AtOutliner.dragAndDropRegion(7, 3);
-
-  // The potential errors should be caught by `errorsCollector` plugin
-});
