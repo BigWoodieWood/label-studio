31d19c5d153838a2c763778124a95d560bce81d2
Ricardo Cabral <ricardo.cabralm@gmail.com>
2025-07-28 07:56:07 -0400

    Removes unused message parameter from maxLength.

--- web/apps/labelstudio/src/components/Form/Validation/Validators.js
+++ web/apps/labelstudio/src/components/Form/Validation/Validators.js
@@ -2,51 +2,51 @@ import { isDefined, isEmptyString } from "../../../utils/helpers";
 import "./Validation.scss";
 
 export const required = (fieldName, value) => {
-  if (!isDefined(value) || isEmptyString(value)) {
-    return "This field is required";
-  }
+	if (!isDefined(value) || isEmptyString(value)) {
+		return "This field is required";
+	}
 };
 
-export const minLength = (min) => (fieldName, value, message) => {
-  if (isDefined(value) && String(value).length < min) {
-    return message || `Must be at least ${min} characters`;
-  }
+export const minLength = (min) => (fieldName, value) => {
+	if (isDefined(value) && String(value).length < min) {
+		return `Must be at least ${min} characters`;
+	}
 };
 
-export const maxLength = (max) => (fieldName, value, message) => {
-  if (isDefined(value) && String(value).length > max) {
-    return message || `Must be no more than ${max} characters`;
-  }
+export const maxLength = (max) => (fieldName, value) => {
+	if (isDefined(value) && String(value).length > max) {
+		return `Must be no more than ${max} characters`;
+	}
 };
 
 export const matchPattern = (pattern) => (fieldName, value) => {
-  pattern = typeof pattern === "string" ? new RegExp(pattern) : pattern;
+	pattern = typeof pattern === "string" ? new RegExp(pattern) : pattern;
 
-  if (!isEmptyString(value) && value.match(pattern) === null) {
-    return `Must match the pattern ${pattern}`;
-  }
+	if (!isEmptyString(value) && value.match(pattern) === null) {
+		return `Must match the pattern ${pattern}`;
+	}
 };
 
 export const json = (fieldName, value) => {
-  const err = "Must be a valid JSON string";
+	const err = "Must be a valid JSON string";
 
-  if (!isDefined(value) || value.trim().length === 0) return;
+	if (!isDefined(value) || value.trim().length === 0) return;
 
-  if (/^(\{|\[)/.test(value) === false || /(\}|\])$/.test(value) === false) {
-    return err;
-  }
+	if (/^(\{|\[)/.test(value) === false || /(\}|\])$/.test(value) === false) {
+		return err;
+	}
 
-  try {
-    JSON.parse(value);
-  } catch (e) {
-    return err;
-  }
+	try {
+		JSON.parse(value);
+	} catch (e) {
+		return err;
+	}
 };
 
 export const regexp = (fieldName, value) => {
-  try {
-    new RegExp(value);
-  } catch (err) {
-    return "Must be a valid regular expression";
-  }
+	try {
+		new RegExp(value);
+	} catch (err) {
+		return "Must be a valid regular expression";
+	}
 };
