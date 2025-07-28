161f871c7768e32aee7a62abc2c492bb61990554
Ricardo Cabral <ricardo.cabralm@gmail.com>
2025-07-28 11:05:15 -0400

    Refactors Form component to improve validation handling and error display. Enhances FormField to support error state and updates Input component to reflect error styles. Adjusts GeneralSettings to trigger validation on blur and refines layout for better user experience.

--- web/apps/labelstudio/src/components/Form/FormField.js
+++ web/apps/labelstudio/src/components/Form/FormField.js
@@ -1,114 +1,128 @@
-import { forwardRef, useCallback, useContext, useEffect, useRef, useState } from "react";
+import {
+	forwardRef,
+	useCallback,
+	useContext,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 import { isDefined } from "../../utils/helpers";
 import { FormContext } from "./FormContext";
 import * as Validators from "./Validation/Validators";
 
 export const FormField = forwardRef(
-  (
-    {
-      label,
-      name,
-      children,
-      required,
-      validate,
-      skip,
-      allowEmpty,
-      protectedValue,
-      skipAutofill,
-      setValue,
-      dependency,
-      validators,
-      ...props
-    },
-    ref,
-  ) => {
-    /**@type {Form} */
-    const context = useContext(FormContext);
-    const [dependencyField, setDependencyField] = useState(null);
+	(
+		{
+			label,
+			name,
+			children,
+			required,
+			validate,
+			skip,
+			allowEmpty,
+			protectedValue,
+			skipAutofill,
+			setValue,
+			dependency,
+			validators,
+			...props
+		},
+		ref,
+	) => {
+		/**@type {Form} */
+		const context = useContext(FormContext);
+		const [dependencyField, setDependencyField] = useState(null);
+		const [error, setError] = useState(null);
 
-    const field = ref ?? useRef();
+		const field = ref ?? useRef();
 
-    const validation = [...(validate ?? [])];
+		const validation = [...(validate ?? [])];
 
-    validators?.forEach?.((validator) => {
-      const [name, value] = validator.split(/:(.+)/).slice(0, 2);
-      const validatorFunc = Validators[name];
+		validators?.forEach?.((validator) => {
+			const [name, value] = validator.split(/:(.+)/).slice(0, 2);
+			const validatorFunc = Validators[name];
 
-      if (isDefined(validatorFunc)) {
-        if (isDefined(value)) {
-          validation.push(validatorFunc(value));
-        } else {
-          validation.push(validatorFunc);
-        }
-      }
-    });
+			if (isDefined(validatorFunc)) {
+				if (isDefined(value)) {
+					validation.push(validatorFunc(value));
+				} else {
+					validation.push(validatorFunc);
+				}
+			}
+		});
 
-    if (required) validation.push(Validators.required);
+		if (required) validation.push(Validators.required);
 
-    useEffect(() => {
-      if (!context || !dependency) return;
+		useEffect(() => {
+			if (!context || !dependency) return;
 
-      let field = null;
-      const dep = context.getFieldContext(dependency);
+			let field = null;
+			const dep = context.getFieldContext(dependency);
 
-      const handler = () => {
-        props.onDependencyChanged?.(dep.field);
-      };
+			const handler = () => {
+				props.onDependencyChanged?.(dep.field);
+			};
 
-      if (dep) {
-        dep.field.addEventListener("change", handler);
-        field = dep.field;
-      } else {
-        console.warn(`Dependency field not found ${dependency}`);
-      }
+			if (dep) {
+				dep.field.addEventListener("change", handler);
+				field = dep.field;
+			} else {
+				console.warn(`Dependency field not found ${dependency}`);
+			}
 
-      setDependencyField(field);
-      return () => dep.field.removeEventListener("change", handler);
-    }, [context, field, dependency]);
+			setDependencyField(field);
+			return () => dep.field.removeEventListener("change", handler);
+		}, [context, field, dependency]);
 
-    const setValueCallback = useCallback(
-      (value) => {
-        if (!field || !field.current) return;
+		const setValueCallback = useCallback(
+			(value) => {
+				if (!field || !field.current) return;
 
-        /**@type {HTMLInputElement|HTMLTextAreaElement} */
-        const input = field.current;
+				/**@type {HTMLInputElement|HTMLTextAreaElement} */
+				const input = field.current;
 
-        if (setValue instanceof Function) {
-          setValue(value);
-        } else if (input.type === "checkbox" || input.type === "radio") {
-          input.checked = value ?? input.checked;
-        } else if (value === null) {
-          input.value = "";
-        } else {
-          input.value = value;
-        }
+				if (setValue instanceof Function) {
+					setValue(value);
+				} else if (input.type === "checkbox" || input.type === "radio") {
+					input.checked = value ?? input.checked;
+				} else if (value === null) {
+					input.value = "";
+				} else {
+					input.value = value;
+				}
 
-        const evt = document.createEvent("HTMLEvents");
+				const evt = document.createEvent("HTMLEvents");
 
-        evt.initEvent("change", false, true);
-        input.dispatchEvent(evt);
-      },
-      [field],
-    );
+				evt.initEvent("change", false, true);
+				input.dispatchEvent(evt);
+			},
+			[field],
+		);
 
-    useEffect(() => {
-      const isProtected = skipAutofill && (!allowEmpty || protectedValue) && field.current.type === "password";
+		useEffect(() => {
+			const isProtected =
+				skipAutofill &&
+				(!allowEmpty || protectedValue) &&
+				field.current.type === "password";
 
-      context?.registerField({
-        label,
-        name,
-        validation,
-        skip,
-        allowEmpty,
-        skipAutofill,
-        isProtected,
-        protectedValue,
-        field: field.current,
-        setValue: setValueCallback,
-      });
-      return () => context?.unregisterField(name);
-    }, [field, setValueCallback]);
+			context?.registerField({
+				label,
+				name,
+				validation,
+				skip,
+				allowEmpty,
+				skipAutofill,
+				isProtected,
+				protectedValue,
+				field: field.current,
+				setValue: setValueCallback,
+				setError,
+			});
+			return () => context?.unregisterField(name);
+		}, [field, setValueCallback]);
 
-    return children(field, dependencyField, context);
-  },
+		return children(field, dependencyField, context, {
+			error: (error?.length ?? 0) > 0,
+		});
+	},
 );
--- web/apps/labelstudio/src/pages/Settings/GeneralSettings.jsx
+++ web/apps/labelstudio/src/pages/Settings/GeneralSettings.jsx
@@ -1,133 +1,191 @@
-import { EnterpriseBadge, Select, Typography } from "@humansignal/ui";
-import { useCallback, useContext } from "react";
-import { Button } from "@humansignal/ui";
+import { Button, EnterpriseBadge, Select, Typography } from "@humansignal/ui";
+import { useCallback, useContext, useRef } from "react";
+import { InlineError } from "../../components/Error/InlineError";
 import { Form, Input, TextArea } from "../../components/Form";
 import { RadioGroup } from "../../components/Form/Elements/RadioGroup/RadioGroup";
+import { HeidiTips } from "../../components/HeidiTips/HeidiTips";
+import { createURL } from "../../components/HeidiTips/utils";
 import { ProjectContext } from "../../providers/ProjectProvider";
 import { Block, Elem } from "../../utils/bem";
-import { HeidiTips } from "../../components/HeidiTips/HeidiTips";
 import { FF_LSDV_E_297, isFF } from "../../utils/feature-flags";
-import { createURL } from "../../components/HeidiTips/utils";
-import { InlineError } from "../../components/Error/InlineError";
 
 export const GeneralSettings = () => {
-  const { project, fetchProject } = useContext(ProjectContext);
+	const { project, fetchProject } = useContext(ProjectContext);
+	const formRef = useRef();
 
-  const updateProject = useCallback(() => {
-    if (project.id) fetchProject(project.id, true);
-  }, [project]);
+	const updateProject = useCallback(() => {
+		if (project.id) fetchProject(project.id, true);
+	}, [project, fetchProject]);
 
-  const colors = ["#FDFDFC", "#FF4C25", "#FF750F", "#ECB800", "#9AC422", "#34988D", "#617ADA", "#CC6FBE"];
+	const colors = [
+		"#FDFDFC",
+		"#FF4C25",
+		"#FF750F",
+		"#ECB800",
+		"#9AC422",
+		"#34988D",
+		"#617ADA",
+		"#CC6FBE",
+	];
 
-  const samplings = [
-    { value: "Sequential", label: "Sequential", description: "Tasks are ordered by Task ID" },
-    { value: "Uniform", label: "Random", description: "Tasks are chosen with uniform random" },
-  ];
+	const samplings = [
+		{
+			value: "Sequential",
+			label: "Sequential",
+			description: "Tasks are ordered by Task ID",
+		},
+		{
+			value: "Uniform",
+			label: "Random",
+			description: "Tasks are chosen with uniform random",
+		},
+	];
 
-  return (
-    <Block name="general-settings">
-      <Elem name={"wrapper"}>
-        <h1>General Settings</h1>
-        <Block name="settings-wrapper">
-          <Form action="updateProject" formData={{ ...project }} params={{ pk: project.id }} onSubmit={updateProject}>
-            <Form.Row columnCount={1} rowGap="16px">
-              <Input
-                name="title"
-                label="Project Name"
-                required
-                validate={[Form.Validator.minLength(3), Form.Validator.maxLength(50)]}
-              />
+	return (
+		<Block name="general-settings">
+			<Elem name={"wrapper"}>
+				<h1>General Settings</h1>
+				<Block name="settings-wrapper">
+					<Form
+						ref={formRef}
+						action="updateProject"
+						formData={{ ...project }}
+						params={{ pk: project.id }}
+						onSubmit={updateProject}
+					>
+						<Form.Row columnCount={1} rowGap="16px">
+							<Input
+								name="title"
+								label="Project Name"
+								required
+								validate={[
+									Form.Validator.minLength(3),
+									Form.Validator.maxLength(50),
+								]}
+								onBlur={() => {
+									// Trigger form validation to show errors immediately
+									formRef.current?.validateFields();
+								}}
+							/>
 
-              <TextArea name="description" label="Description" style={{ minHeight: 128 }} />
-              {isFF(FF_LSDV_E_297) && (
-                <Block name="workspace-placeholder">
-                  <Elem name="badge-wrapper">
-                    <Elem name="title">Workspace</Elem>
-                    <EnterpriseBadge className="ml-2" />
-                  </Elem>
-                  <Select placeholder="Select an option" disabled options={[]} />
-                  <Typography size="small" className="my-tight">
-                    Simplify project management by organizing projects into workspaces.{" "}
-                    <a
-                      target="_blank"
-                      href={createURL(
-                        "https://docs.humansignal.com/guide/manage_projects#Create-workspaces-to-organize-projects",
-                        {
-                          experiment: "project_settings_tip",
-                          treatment: "simplify_project_management",
-                        },
-                      )}
-                      rel="noreferrer"
-                      className="underline hover:no-underline"
-                    >
-                      Learn more
-                    </a>
-                  </Typography>
-                </Block>
-              )}
-              <RadioGroup name="color" label="Color" size="large" labelProps={{ size: "large" }}>
-                {colors.map((color) => (
-                  <RadioGroup.Button key={color} value={color}>
-                    <Block name="color" style={{ "--background": color }} />
-                  </RadioGroup.Button>
-                ))}
-              </RadioGroup>
+							<TextArea
+								name="description"
+								label="Description"
+								style={{ minHeight: 128 }}
+							/>
+							{isFF(FF_LSDV_E_297) && (
+								<Block name="workspace-placeholder">
+									<Elem name="badge-wrapper">
+										<Elem name="title">Workspace</Elem>
+										<EnterpriseBadge className="ml-2" />
+									</Elem>
+									<Select
+										placeholder="Select an option"
+										disabled
+										options={[]}
+									/>
+									<Typography size="small" className="my-tight">
+										Simplify project management by organizing projects into
+										workspaces.{" "}
+										<a
+											target="_blank"
+											href={createURL(
+												"https://docs.humansignal.com/guide/manage_projects#Create-workspaces-to-organize-projects",
+												{
+													experiment: "project_settings_tip",
+													treatment: "simplify_project_management",
+												},
+											)}
+											rel="noreferrer"
+											className="underline hover:no-underline"
+										>
+											Learn more
+										</a>
+									</Typography>
+								</Block>
+							)}
+							<RadioGroup
+								name="color"
+								label="Color"
+								size="large"
+								labelProps={{ size: "large" }}
+							>
+								{colors.map((color) => (
+									<RadioGroup.Button key={color} value={color}>
+										<Block name="color" style={{ "--background": color }} />
+									</RadioGroup.Button>
+								))}
+							</RadioGroup>
 
-              <RadioGroup label="Task Sampling" labelProps={{ size: "large" }} name="sampling" simple>
-                {samplings.map(({ value, label, description }) => (
-                  <RadioGroup.Button
-                    key={value}
-                    value={`${value} sampling`}
-                    label={`${label} sampling`}
-                    description={description}
-                  />
-                ))}
-                {isFF(FF_LSDV_E_297) && (
-                  <RadioGroup.Button
-                    key="uncertainty-sampling"
-                    value=""
-                    label={
-                      <>
-                        Uncertainty sampling <EnterpriseBadge className="ml-2" />
-                      </>
-                    }
-                    disabled
-                    description={
-                      <>
-                        Tasks are chosen according to model uncertainty score (active learning mode).{" "}
-                        <a
-                          target="_blank"
-                          href={createURL("https://docs.humansignal.com/guide/active_learning", {
-                            experiment: "project_settings_workspace",
-                            treatment: "workspaces",
-                          })}
-                          rel="noreferrer"
-                        >
-                          Learn more
-                        </a>
-                      </>
-                    }
-                  />
-                )}
-              </RadioGroup>
-            </Form.Row>
+							<RadioGroup
+								label="Task Sampling"
+								labelProps={{ size: "large" }}
+								name="sampling"
+								simple
+							>
+								{samplings.map(({ value, label, description }) => (
+									<RadioGroup.Button
+										key={value}
+										value={`${value} sampling`}
+										label={`${label} sampling`}
+										description={description}
+									/>
+								))}
+								{isFF(FF_LSDV_E_297) && (
+									<RadioGroup.Button
+										key="uncertainty-sampling"
+										value=""
+										label={
+											<>
+												Uncertainty sampling{" "}
+												<EnterpriseBadge className="ml-2" />
+											</>
+										}
+										disabled
+										description={
+											<>
+												Tasks are chosen according to model uncertainty score
+												(active learning mode).{" "}
+												<a
+													target="_blank"
+													href={createURL(
+														"https://docs.humansignal.com/guide/active_learning",
+														{
+															experiment: "project_settings_workspace",
+															treatment: "workspaces",
+														},
+													)}
+													rel="noreferrer"
+												>
+													Learn more
+												</a>
+											</>
+										}
+									/>
+								)}
+							</RadioGroup>
+						</Form.Row>
 
-            <Form.Actions>
-              <Form.Indicator>
-                <span case="success">Saved!</span>
-              </Form.Indicator>
-              <Button type="submit" className="w-[150px]" aria-label="Save general settings">
-                Save
-              </Button>
-            </Form.Actions>
+						<Form.Actions>
+							<Form.Indicator>
+								<span case="success">Saved!</span>
+							</Form.Indicator>
+							<Button
+								type="submit"
+								className="w-[150px]"
+								aria-label="Save general settings"
+							>
+								Save
+							</Button>
+						</Form.Actions>
 
-            <InlineError />
-          </Form>
-        </Block>
-      </Elem>
-      {isFF(FF_LSDV_E_297) && <HeidiTips collection="projectSettings" />}
-    </Block>
-  );
+						<InlineError />
+					</Form>
+				</Block>
+			</Elem>
+			{isFF(FF_LSDV_E_297) && <HeidiTips collection="projectSettings" />}
+		</Block>
+	);
 };
 
 GeneralSettings.menuItem = "General";
