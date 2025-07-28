379707bfec8a1ef7fc8996015d1df9f3f8e88181
Ricardo Cabral <ricardo.cabralm@gmail.com>
2025-07-28 11:46:45 -0400

    Linter fixes.

--- web/apps/labelstudio/src/components/Form/Elements/Input/Input.jsx
+++ web/apps/labelstudio/src/components/Form/Elements/Input/Input.jsx
@@ -4,57 +4,45 @@ import { default as Label } from "../Label/Label";
 import "./Input.scss";
 
 const Input = ({
-	label,
-	description,
-	footer,
-	className,
-	validate,
-	required,
-	skip,
-	labelProps,
-	ghost,
-	error,
-	tooltip,
-	tooltipIcon,
-	...props
+  label,
+  description,
+  footer,
+  className,
+  validate,
+  required,
+  skip,
+  labelProps,
+  ghost,
+  error,
+  tooltip,
+  tooltipIcon,
+  ...props
 }) => {
-	const input = (
-		<FormField
-			label={label}
-			name={props.name}
-			validate={validate}
-			required={required}
-			skip={skip}
-			{...props}
-		>
-			{(ref, dependencyField, context, fieldProps) => {
-				const hasError = error || fieldProps?.error;
-				const finalClassList = [
-					cn("input-ls").mod({ ghost, error: hasError }),
-					className,
-				]
-					.join(" ")
-					.trim();
-				return <input {...props} ref={ref} className={finalClassList} />;
-			}}
-		</FormField>
-	);
+  const input = (
+    <FormField label={label} name={props.name} validate={validate} required={required} skip={skip} {...props}>
+      {(ref, dependencyField, context, fieldProps) => {
+        const hasError = error || fieldProps?.error;
+        const finalClassList = [cn("input-ls").mod({ ghost, error: hasError }), className].join(" ").trim();
+        return <input {...props} ref={ref} className={finalClassList} />;
+      }}
+    </FormField>
+  );
 
-	return label ? (
-		<Label
-			{...(labelProps ?? {})}
-			description={description}
-			footer={footer}
-			text={label}
-			tooltip={tooltip}
-			tooltipIcon={tooltipIcon}
-			required={required}
-		>
-			{input}
-		</Label>
-	) : (
-		input
-	);
+  return label ? (
+    <Label
+      {...(labelProps ?? {})}
+      description={description}
+      footer={footer}
+      text={label}
+      tooltip={tooltip}
+      tooltipIcon={tooltipIcon}
+      required={required}
+    >
+      {input}
+    </Label>
+  ) : (
+    input
+  );
 };
 
 export default Input;
