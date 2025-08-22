30f58ba2b932e3c829c763978802724da4079e3d
hlomzik <hlomzik@gmail.com>
2025-08-22 15:32:22 +0100

    Add ChatRegion docs

---
+++ web/libs/editor/src/regions/ChatRegion.js
@@ -0,0 +1,19 @@
+// stub file to keep ChatRegion docs
+
+/**
+ * @example
+ * {
+ *   "value": {
+ *     "chatmessage": {
+ *       "role": "user",
+ *       "content": "Hello, how are you?",
+ *       "timestamp": 1755872989436
+ *     }
+ *   }
+ * }
+ * @typedef {Object} ChatRegionResult
+ * @property {Object} value
+ * @property {string} value.chatmessage.role role of the message (user, assistant, system)
+ * @property {string} value.chatmessage.content content of the message
+ * @property {number} value.chatmessage.timestamp timestamp of the message (ms)
+ */
